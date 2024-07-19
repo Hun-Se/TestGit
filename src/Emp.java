d8a31cb180c8c2441216bfd89ee90ed83cf0d5fe
soo004 <miroplay04@gmail.com>
2024-07-19 21:56:50 +0900

    기능 123 완료

---
+++ src/Emp.java
@@ -0,0 +1,16 @@
+import java.util.Date;
+
+public class Emp extends Department {
+	
+   String name;
+   Date hireDate;
+   double salary;
+   String email;
+   String phoneNumber;
+   String jobId;
+   double commission ;
+   int managerId;
+   int employeeId;
+   
+
+}
